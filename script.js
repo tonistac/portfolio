c0b72f832814898f6683d11451ba36d01157905c
chioak <dokes3@live.com>
2022-10-29 07:40:54 -0400

    Designed the project

--- script.js
+++ script.js
@@ -2,13 +2,51 @@
 const mouseCircle = document.querySelector(".mouse-circle");
 const mouseDot = document.querySelector(".mouse-dot");
 
-
 const mouseCircleFn = (x, y) => {
-  
-  mouseCircle.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`;
-
+  mouseCircle.style.cssText = `top: ${y}px; left: ${x}px; 
+  opacity: 1`;
   mouseDot.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`;
 };
+//End of Mouse Circle
+
+//Animated Circles
+const circles = document.querySelectorAll('.circle')
+const mainImg = document.querySelector(".main-circle img");
+
+let mX = 0;
+let mY = 0; 
+const z = 100;
+
+const animateCircles = (e,x,y)=>{
+if(x < mX) {
+  circles.forEach((circle)=>{
+    circle.style.left = `${z}px`;
+  });
+  mainImg.style.left = `${z}px`;
+} else if(x > mX) {
+  circles.forEach((circle)=>{
+    circle.style.left = `-${z}px`;
+  });
+  mainImg.style.left = `-${z}px`;
+}
+
+if(x < mY) {
+  circles.forEach((circle)=>{
+    circle.style.top = `${z}px`;
+  });
+  mainImg.style.top = `${z}px`;
+} else if (x > mY){
+  circles.forEach((circle)=>{
+    circle.style.top = `-${z}px`;
+  });
+  mainImg.style.top = `-${z}px`;
+}
+
+  mX = e.clientX
+  mY = e.clientY
+}
+
+//End of Animated Circles
 
 
 document.body.addEventListener('mousemove', (e) =>{
@@ -16,10 +54,70 @@ let x = e.clientX;
 let y = e.clientY;
 
 mouseCircleFn(x,y);
+animateCircles(e,x,y);
 });
 
 document.body.addEventListener('mouseleave', () => {
     mouseCircle.style.opacity = "0";
     mouseDot.style.opacity = "0";
 });
+
+// Main Button
+const mainBtns = document.querySelectorAll('.main-btn');
+
+mainBtns.forEach((btn) =>{
+  let ripple;
+
+btn.addEventListener('mouseenter',(e) =>{
+  const left = e.clientX - e.target.getBoundingClientRect().left;
+  const top = e.clientY - e.target.getBoundingClientRect().top;
+
+  ripple = document.createElement("div");
+  ripple.classList.add("ripple");
+  ripple.style.left = `${left}px`;
+  ripple.style.top = `${top}px`;
+  btn.prepend(ripple);
+});
+
+btn.addEventListener('mouseleave',() => {
+  btn.removeChild(ripple);
+});
+});
+
+
+
+// End of main button //
+
+//About me text
+const aboutMeText = document.querySelector(".about-me-text");
+const aboutMeTextContent = "I am a designer & I create award winning websites with the best user experience & I do not talk too much, just reach out to me :)";
+
+Array.from(aboutMeTextContent).forEach(char =>{
+  const span = document.createElement('span');
+  span.textContent = char;
+  aboutMeText.appendChild(span);
+
+  span.addEventListener("mouseenter", (e) =>{
+    e.target.style.animation = "aboutMeTextAnim 10s infinite";
+  });
+});
+// End of about me Text
+
+
+//Projects
+
+//End of Projects
+const projects  = document.querySelectorAll('.project')
+projects.forEach(project => {
+  project.addEventListener('mouseenter',() =>{
+    project.firstElementChild.style.top = `-${
+      project.firstElementChild.offsetHeight - project.
+      offsetHeight + 20
+     }px`;
+  });
+
+  project.addEventListener("mouseleave", ()=>{
+    project.firstElementChild.style.top = "2rem";
+  });
+})
 //End of Mouse Circle
